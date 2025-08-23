87f302503f95ec951a703b9a2ae677ac28e100c5
Tom George <tomgeorge12@gmail.com>
2025-08-23 18:01:58 +0530

    vite config updated

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,23 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+    build: {
+        outDir: 'build',
     },
-  },
+    plugins: [
+        react()
+    ],
+    resolve: {
+        alias: {
+        "@": path.resolve(__dirname, "src"),
+        }
+    },
+    server: {
+        port: 3000,
+        open: true,
+        hmr: {
+            host: 'localhost',
+            clientPort: 3000
+        }
+    }
 });
